32cee72cca1afb1e33abae79aad1825b2ad2778d
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-04 18:09:03 +0630

    service/auth: remove requireAuth middleware from currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -8,7 +8,6 @@ const router = express.Router();
 router.get(
   '/api/users/currentuser',
   currentUser,
-  requireAuth,
   (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
   }
