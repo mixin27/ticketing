7cc6e6a8bea487f71e1dd88d4c424d18b8722021
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-10 23:31:35 +0630

    service/auth: change db connect log message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -12,8 +12,8 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to MongoDB!');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
   }
