0b6b1df9794995c22896515e19288ce36ee25f14
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-24 00:54:37 +0630

    service/expiration: add await keyword to publisher

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -14,7 +14,7 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 });
 
 expirationQueue.process(async (job) => {
-  new ExpirationCompletePublisher(natsWrapper.client).publish({
+  await new ExpirationCompletePublisher(natsWrapper.client).publish({
     orderId: job.data.orderId,
   });
 });
