7063b273cf1671938bbc777b315adf8b744f9c33
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-26 20:51:12 +0630

    service/orders: update starting up log message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
-  console.log('Starting up orders service...');
+  console.log('Starting up the orders service...');
 
   if (!process.env.MONGO_URI)
     throw new Error('MONGO_URI env must be specified');
