676446f7bce15fa896326dbb7f1e214569e95aa0
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-01-26 18:32:58 +0630

    service/auth: add starting service log message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  console.log('Starting auth service up...');
+
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI env must be specified');
   }
